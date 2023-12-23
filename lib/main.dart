8403090582b1676b40f5a8afb2c4fa570d9873e1
Liam Ryde <liamryde@gmail.com>
2023-12-23 15:27:07 -0500

    Delete unused files, update dependencies, refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,41 @@
-/*
-Copyright 2021 The dahliaOS Authors
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:terminal/views/terminal_frame.dart';
-import 'package:terminal/views/terminal_settings.dart';
+import 'package:terminal/interface.dart';
 import 'package:zenit_ui/zenit_ui.dart';
 
-void main() {
-  runApp(const Terminal());
+bool get isDesktop {
+  if (kIsWeb) return false;
+  return [
+    TargetPlatform.windows,
+    TargetPlatform.linux,
+    TargetPlatform.macOS,
+  ].contains(defaultTargetPlatform);
 }
 
-class Terminal extends StatelessWidget {
-  const Terminal({Key? key}) : super(key: key);
+void main() {
+  runApp(const TerminalApp());
+
+  doWhenWindowReady(() {
+    const initialSize = Size(600, 450);
+    appWindow.minSize = initialSize;
+    appWindow.size = initialSize;
+    appWindow.alignment = Alignment.center;
+    appWindow.show();
+  });
+}
+
+class TerminalApp extends StatelessWidget {
+  const TerminalApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    //setWindowTitle("Terminal");
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Terminal',
-      initialRoute: '/',
-      theme: ThemeEngine.zenitDefaultLightTheme,
-      darkTheme: ThemeEngine.zenitDefaultDarkTheme,
-      themeMode: ThemeMode.dark,
-      routes: {
-        "/": (context) => const TerminalFrame(),
-        "/settings": (context) => const TerminalSettings(),
-      },
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Terminal',
+        theme: createZenitTheme(),
+        darkTheme: createZenitTheme(brightness: Brightness.dark),
+        themeMode: ThemeMode.dark,
+        home: const Interface());
   }
 }
